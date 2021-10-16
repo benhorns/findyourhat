0792c948a58273428bfd05b14e17fc0b753c4e79
Ben Horns <benhorns@gmail.com>
2021-10-16 14:06:12 -0400

    getting there

--- main.js
+++ main.js
@@ -20,7 +20,11 @@ class Field {
     }
 
     getMove() {
+        console.log('')
         console.log(this.print())
+        console.log(this.field.length)
+        console.log(this.field[0].length)
+        // console.log(this.field[0].length)
         const input = prompt('What is your move? u: up, d: down, l: left, r: right ');
         switch(input) {
             case 'u':
@@ -35,11 +39,11 @@ class Field {
                 break;
                 case 'd':
                     xCoord +=1
-                    if (xCoord <= 2) {
+                    if (xCoord <= this.field.length) {
                         this.movingTo(xCoord)
                     } else {
                         console.log('Out of bound!')
-                        xCoord = 2
+                        xCoord = this.field.length
                         this.getMove()
                     }
                 break;
@@ -55,11 +59,11 @@ class Field {
                     break;
                     case 'r':
                         yCoord +=1
-                        if (yCoord <= 2) {
+                        if (yCoord <= this.field[0].length) {
                             this.movingTo(xCoord, yCoord)
                         } else {
                             console.log('Out of bounds!')
-                            Coord = 2
+                            yCoord = this.field[0].length
                             this.getMove()
                     }
                 break;
@@ -76,53 +80,64 @@ class Field {
                             this.getMove()
                         } else {
                             if (this.field[xCoord][yCoord] === hat ) {
-                        
                             console.log('Congratulations you found your hat!')
                             this.field[xCoord][yCoord] = '!'
                             console.log(this.print())
-                            } else {
+                        } else {
                             console.log('Oops you fell down a hole.')
                             this.field[xCoord][yCoord] = 'X'
                             console.log(this.print())
                         }
-                    }
-                }
-
+                        const input = prompt('Want to play again? y/n');
+                        switch(input) {
+                            case 'y':
+                                const r = parseInt(prompt('How many rows do you want on the board?'))
+                                const c = parseInt(prompt('How many columns do you want on the board?'))
+                                const p = parseInt(prompt('How many per do you want on the board?'))
+                                this.field = Field.generateField(r,c,p)
+                                xCoord = 0;
+                                yCoord = 0;
+                                this.getMove()
+                                break;
+                                case 'n':
+                                    console.log('Okay thanks for playing!')
+                                    break;
+                                }
+                            }
+                        }
+                        
                 static generateField (rows, cols, percentage) {
-                    let tmpArr = []
                     let arr = []
-                    let freq = (rows*cols) * (percentage/100)
-                    let randRow = Math.Floor(Math.random() * rows);
-                    let randCol = Math.Floor(Math.random() * cols);
-                    console.log(randRow)
-                    console.log(randRow)
-                    console.log(randRow)
-                    console.log(randRow)
-                    console.log(randCol)
-                    console.log(randCol)
-                    console.log(randCol)
-                    // console.log(typeof rows)
-                    // console.log(typeof cols)
-                    // console.log(typeof percentage)
-                    // console.log(typeof freq)
-                    // console.log(freq)
-                    for (let i = 0; i < rows*cols - freq; i++) {
-                        tmpArr.push(fieldCharacter)
+                    let freq = Math.ceil((rows*cols) * (percentage/100))
+                    console.log(arr.length)
+                    for (let i = 0; i < rows; i++) {
+                        arr.push( new Array(cols).fill(fieldCharacter))
+                    }   
+                    arr[0][0] = pathCharacter;
+                    console.log(Math.ceil(freq))
+                    let i = 0;
+                    do {
+                        let randRow = (Math.floor(Math.random() * rows))
+                        let randCol = (Math.floor(Math.random() * cols))
+                        if (arr[randRow][randCol] != hole && arr[randRow][randCol] != pathCharacter) {
+                            arr[randRow][randCol] = hole;
+                            i++;
+                        }
                     }
-                    for (let i =0; i < freq; i++) {
-                        let index = Math.round(Math.random() * (tmpArr.length - 1));
-                        tmpArr.splice(index,0, hole)
+                    while (i < freq) 
+                    i = 0;
+                    do {
+                        let randRow = (Math.floor(Math.random() * rows))
+                        let randCol = (Math.floor(Math.random() * cols))
+                        if (arr[randRow][randCol] != hole && arr[randRow][randCol] != pathCharacter) {
+                            arr[randRow][randCol] = hat;
+                            i++;
+                        }
                     }
-                    for (var i = 0; i < rows; i++) {
-                        let row = tmpArr.slice( i * cols, (i + 1) * rows);
-                        arr.push(row);
-                    }
-                    if (arr[(Math.floor(Math.random() * rows))][(Math.floor(Math.random() * cols))] !== hole) {
-
-                    } 
-                    // arr[(Math.floor(Math.random() * rows))][(Math.floor(Math.random() * cols))] = 'P'
+                    while (i < 1) 
                     return arr
                 }
+
             }
                 
                 // const myField = new Field([
@@ -133,9 +148,10 @@ class Field {
                 
                 // myField.getMove()
 
-                const myOtherField = new Field(Field.generateField(3,3,20))
-                console.log(myOtherField.print())
-                
+                const myOtherField = new Field(Field.generateField(4,8,20))
+                // console.log(myOtherField.field)
+                myOtherField.getMove()
+                    
 
 
 
